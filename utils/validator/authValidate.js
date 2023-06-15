8a80f81c907d6d050d117eeb40a802d1c112a052
omarmokhtar01 <91415360+omarmokhtar01@users.noreply.github.com>
2023-06-15 16:40:11 +0300

    valid

--- utils/validator/authValidate.js
+++ utils/validator/authValidate.js
@@ -1,21 +1,8 @@
-const { check, body } = require("express-validator");
-const { default: slugify } = require("slugify");
+const { check } = require("express-validator");
 const userModel = require("../../models/userModel");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
 
 exports.ruleSignUpValidator = [
-  // Check Name
-  check("name")
-    .notEmpty()
-    .withMessage("Should Not Empty")
-    .isString()
-    .withMessage("Should be String")
-    .isLength({ min: 3, max: 32 })
-    .withMessage("Invalid Length Name between 3 , 32 charactar"),
-  body("name").custom((val, { req }) => {
-    req.body.slug = slugify(val);
-    return true;
-  }),
   // Check Email
   check("email")
     .notEmpty()
@@ -25,7 +12,7 @@ exports.ruleSignUpValidator = [
     .custom((value) => {
       return userModel.findOne({ email: value }).then((user) => {
         if (user) {
-          return Promise.reject("E-mail already in use");
+          return Promise.reject("يوجد حساب بالبريد الإلكتروني, الرجاء تسجيل الدخول");
         }
       });
     }),
@@ -33,47 +20,22 @@ exports.ruleSignUpValidator = [
   check("password")
     .notEmpty()
     .withMessage("Password is Required")
-    .isLength({ min: 6 }),
+    .isLength({ min: 6 })
+    .withMessage("لا تقل كلمة السر عن ستة أحرف")
+  ,
   // Check Phone
   check("phone")
     .notEmpty()
     .withMessage("phone is Required")
     .isMobilePhone("ar-EG")
-    .withMessage("Please enter egyptian number"),
+    .withMessage("يجب ادخال رقم مصري"),
   // Check nationalID
   check("nationalID")
     .notEmpty()
     .withMessage("National ID is Required")
     .isString()
     .isLength({ min: 14, max: 14 })
-    .withMessage("Please enter valid national ID"),
-  // location
-  check("location")
-    .notEmpty()
-    .withMessage("location is Required")
-    .isString()
-    .isLength({ min: 3, max: 32 })
-    .withMessage("location should be String"),
-  // birthDate
-  check("birthDate")
-    .notEmpty()
-    .withMessage('birthday Should Not Empty')
-    .isISO8601()
-    .toDate()
-    .withMessage("birthday should be Date yy-mm-dd"),
-  // bloodType
-  check("bloodType")
-    .notEmpty()
-    .withMessage('BloodType Should Not Empty')
-    .isString()
-    .withMessage("BloodType should be String").custom((val) => {
-      let arr = ["AB+", "AB-", "A+", "A-", "B+", "B-", "O+", "O-", "لا أعلم"];
-      if (!arr.includes(val)) {
-        return Promise.reject('Data Should be from "AB+", "AB-", "A+", "A-", "B+", "B-", "O+", "O-", "لا أعلم"')
-      }
-      return true;
-    }),
-
+    .withMessage("يجب ادخال رقم قومى صحيح"),
   validatorMiddleware,
 ];
 
